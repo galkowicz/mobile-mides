cc3814a9fc089a2987a60fcf64ef9b0a7b0d60bb
orian_galkowicz <orian.galkowicz@ladbrokescoarl.com>
2017-05-14 17:44:03 +0300

    menu animation (not finished) menu markup refactoring

--- midess_files/myscript.js
+++ midess_files/myscript.js
@@ -1,13 +1,26 @@
-$(document).ready(function() {
-	$('meta[name=viewport]').attr('content', 'width=device-width,initial-scale=1,maximum-scale=1.0');
-	
-	if(! /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
- // some code..
-window.location = "http://mides.co.il/";
-}
+$(document).ready(function () {
+    $('meta[name=viewport]').attr('content', 'width=device-width,initial-scale=1,maximum-scale=1.0');
 
-    $( "#hebMenuButton" ).click(function() {
-        $("#hebMenu").load("midess_files/hebrew-menu.html");
-    });
+    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        // some code..
+        window.location = "http://mides.co.il/";
+    }
+
+    function attachEvents() {
+
+        $("#hebMenuButton").click(function () {
+            $("#hebMenu").load("midess_files/hebrew-menu.html");
+        });
+
+        var burgerMenu = $(".burger-menu-icon");
+        burgerMenu.click(function () {
+            burgerMenu.toggleClass('menuClosed');
+            burgerMenu.toggleClass('menuOpen');
+        });
+    }
+
+    attachEvents();
+
+
 });
 
